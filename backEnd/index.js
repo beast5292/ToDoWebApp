1fca639714b1ebf441a110cdc96bc925a26710b9
Mindula <mindula5292@gmail.com>
2024-07-30 12:05:19 +0530

    json to send

--- backEnd/index.js
+++ backEnd/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/', (req, res) => {
-    res.json({ message: 'Default route' });
+    res.send('Default route');
 });
 
 // User POST route
